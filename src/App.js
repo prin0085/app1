1d5c22f5b90ac3f4c8d7dbb45a496fbc86051646
parin <tronraganzy5678@gmail.com>
2023-12-25 11:40:26 +0700

    add style.css and update app.js external css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powderblue',
-    fontSize: 'larger',
-    padding: '10px',
-  }
-  return <div style={divStyle}>Hello React</div>
+  return (
+    <>
+      <h2>DeveloperThai.com</h2>
+      <div className='title'>React & React Native</div>
+    </>
+  )
 }
 
 export default App;
