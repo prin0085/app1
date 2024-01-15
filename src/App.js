7617c782be79dc5e93690e1b7d82b0279c30b4b2
parin <tronraganzy5678@gmail.com>
2024-01-15 11:31:21 +0700

    add event-data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
-import { Calculator2 } from './calculator';
+import { EventData2 } from './event-data';
 
 function App() {
-  return <Calculator2 />
+  return <EventData2 />
 }
 
 export default App;
