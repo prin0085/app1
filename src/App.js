e7e617d16d26c99c45095fa2c494fdbff0763778
parin <tronraganzy5678@gmail.com>
2024-01-19 14:28:07 +0700

    add refs-array
    add table of useref array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
-import RefsFunc from './refs-func';
+import RefsArray from './refs-array';
 
 function App() {
-  return <RefsFunc />
+  return <RefsArray />
 }
 
 export default App;
